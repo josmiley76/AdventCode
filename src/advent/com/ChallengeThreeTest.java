b308721c16c1af9e0b45809cde5a6f6bd08a78ea
joanne day <joanne.day@sky.uk>
2022-01-17 20:42:55 +0000

    Fixed tests but needs to static class changing

--- src/advent/com/ChallengeThreeTest.java
+++ src/advent/com/ChallengeThreeTest.java
@@ -1,20 +1,27 @@
 package advent.com;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChallengeThreeTest {
+    @BeforeEach
+    public void setup(){
+        int[] binaryStateCountForOnes = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] binaryStateCountForZeros = {0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0};
 
-    private static int[] binaryStateCountsForOnes = {0,0,0,0,0};
-    private static int[] binaryStateCountsForZeros = {0,0,0,0,0};
+        ChallengeThree.setBinaryStateCountForOnes(binaryStateCountForOnes);
+        ChallengeThree.setBinaryStateCountForZeros(binaryStateCountForZeros);
+
+    }
 
     @Test
     public void ShouldCalculateBinaryStates(){
-        final int[] TEST_VALUES = {16, 9, 4, 3, 1};
-        final int[] EXPECTED_BINARY_STATE_COUNT_FOR_ONES = {1, 1, 1, 1, 3};
-        final int[] EXPECTED_BINARY_STATE_COUNT_FOR_ZEROS = {4, 4, 4, 4, 2};
+        final int[] TEST_VALUES = { 2048,1024 ,512, 256, 128, 64, 32, 16, 9, 4, 3, 1};
+        final int[] EXPECTED_BINARY_STATE_COUNT_FOR_ONES = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3};
+        final int[] EXPECTED_BINARY_STATE_COUNT_FOR_ZEROS = {11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 9};
         ChallengeThree.calculateBinaryStateCounts(TEST_VALUES);
         assertArrayEquals(EXPECTED_BINARY_STATE_COUNT_FOR_ONES, ChallengeThree.getBinaryStateCountForOnes());
         assertArrayEquals(EXPECTED_BINARY_STATE_COUNT_FOR_ZEROS, ChallengeThree.getBinaryStateCountForZeros());
@@ -23,8 +30,8 @@ class ChallengeThreeTest {
 
     @Test
     public void ShouldCalculateGammaRate(){
-        final int GAMMA_RATE = 0x12;
-        final int[] TEST_VALUES = {18, 25, 22, 23, 8};
+        final int GAMMA_RATE = 0x18;
+        final int[] TEST_VALUES = {1032, 25, 22, 23, 8};
 
         ChallengeThree.calculateBinaryStateCounts(TEST_VALUES);
         assertEquals(GAMMA_RATE,ChallengeThree.calculateGammaRate());
@@ -32,8 +39,8 @@ class ChallengeThreeTest {
 
     @Test
     public void ShouldCalculateEpsilonRate(){
-        final int EPSILON_RATE = 0x0D;
-        final int[] TEST_VALUES = {18, 25, 22, 23, 8};
+        final int EPSILON_RATE = 0xFE7;
+        final int[] TEST_VALUES = {1032, 25, 22, 23, 8};
 
         ChallengeThree.calculateBinaryStateCounts(TEST_VALUES);
         assertEquals(EPSILON_RATE,ChallengeThree.calculateEpsilonRate());
@@ -41,8 +48,8 @@ class ChallengeThreeTest {
 
     @Test
     public void ShouldCalculatePowerConsumption(){
-        final int[] TEST_VALUES = {18, 25, 22, 23, 8};
-        final int POWER_CONSUMPTION = 0x12 * 0x0D;
+        final int[] TEST_VALUES = {1032, 25, 22, 23, 8};
+        final int POWER_CONSUMPTION = 0x18 * 0xFE7;
 
         ChallengeThree.calculateBinaryStateCounts(TEST_VALUES);
         int powerConsumption = ChallengeThree.calculateGammaRate() * ChallengeThree.calculateEpsilonRate();
